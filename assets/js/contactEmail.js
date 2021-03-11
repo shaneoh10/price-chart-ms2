66339faa2081037ab65b116da0790a6d30767980
shaneoh10 <shaneaghada@gmail.com>
2021-03-11 09:28:41 +0000

    feat: Add email.js functionality to contact.html

---
+++ assets/js/contactEmail.js
@@ -0,0 +1,19 @@
+// Code used from Code Institute UCD resume walkthrough project lectures
+
+function sendMail(contactForm) {
+    emailjs.send('gmail', 'PriceChart', {
+        'from_name': contactForm.name.value,
+        'from_email': contactForm.emailaddress.value,
+        'message': contactForm.message.value
+    })
+    .then(
+        function(response) {
+            console.log('SUCCESS', response);
+            document.getElementById('form').innerHTML = `<h2 class="text-center">Thanks for your message!</h2>`;
+        },
+        function(error) {
+            console.log('FAILED', error);
+        });
+
+    return false;    
+}
