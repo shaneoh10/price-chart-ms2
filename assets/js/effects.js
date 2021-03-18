05559052fd31753f1dc460a1c655a9ab9189b276
shaneoh10 <shaneaghada@gmail.com>
2021-03-18 18:13:56 +0000

    feat: Add effects.js javascript effects to all pages

---
+++ assets/js/effects.js
@@ -0,0 +1,11 @@
+// // Navbar collapse on page click
+// // https://mdbootstrap.com/support/general/auto-close-navbar-when-click-on-link-responsive-mode/
+
+$(document).click(function () {
+    $('.navbar-collapse').collapse('hide');
+});
+
+// Divs appear on scroll down
+// https://scrollrevealjs.org/guide/whats-new.html
+
+ScrollReveal().reveal('.chart-box', { duration: 2000, delay: 100 });
